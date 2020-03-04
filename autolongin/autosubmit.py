3423004c3648aaad9b51e8fc7b8c07a59b5f2285
nv4dll-git <nv4dll@outlook.com>
2020-03-04 09:25:50 +0800

    add log wirter

--- autolongin/autosubmit.py
+++ autolongin/autosubmit.py
@@ -105,7 +105,7 @@ data2mmy={
     'Other': '',
     'GetAreaUrl': '/SPCP/Web/Report/GetArea',
     'Sex': '女',
-    'IdCard': '13098219951104822x',
+    'IdCard': '13098219951104822X',
     'SpeType': 'S',
     'CollegeNo': '000001',
     'SpeGrade': '2018',
@@ -188,11 +188,11 @@ data2qn={
     'Province': '620000',
     'City':'621000',
     'County':'621002',
-    'ComeWhere': '长庆龙凤园九区',
+    'ComeWhere': '科教新村',
     'FaProvince': '620000',
     'FaCity': '621000',
     'FaCounty': '621002',
-    'FaComeWhere': '长庆龙凤园九区',
+    'FaComeWhere': '科教新村',
     'radio_1': '2e856fa0-5b66-4ba5-97a9-177b4b77e2b3',
     'radio_2': 'ee6648b4-3da8-41ae-b769-04a8d29519e1',
     'radio_3': 'd4b03707-0619-4dfb-82c3-c1f2af3bfb53',
@@ -207,7 +207,7 @@ data2qn={
     'Other': ' ',
     'GetAreaUrl': '/SPCP/Web/Report/GetArea',
     'Sex': '男',
-    'IdCard': '421087199608290031',
+    'IdCard': '62282719960103003X',
     'SpeType': 'S',
     'CollegeNo': '000001',
     'SpeGrade': '2018',
@@ -276,7 +276,7 @@ data2fj={
     'PZData': '[{"OptionName":"假期离校回家，目前尚未返校","SelectId":"2e856fa0-5b66-4ba5-97a9-177b4b77e2b3","TitleId":"6564c529-c9ce-4c9c-8158-fcbf53b7ef9a","OptionType":"0"},{"OptionName":"返乡至湖北其他地区(除武汉)","SelectId":"33775f86-44af-4d84-acbe-ddc5f884a48b","TitleId":"0f9eba15-972e-47ce-aff7-691f0c802b78","OptionType":"0"},{"OptionName":"无","SelectId":"d4b03707-0619-4dfb-82c3-c1f2af3bfb53","TitleId":"35eec330-3122-4054-a4ac-fe8ac95135df","OptionType":"0"},{"OptionName":"有","SelectId":"6545bf6d-6b84-4efc-950c-8e79a3eb5b90","TitleId":"bd1fef27-db0a-4898-8963-8b8e34714dfc","OptionType":"0"},{"OptionName":"无","SelectId":"24d314e6-d37d-448d-897e-1910b30f8da1","TitleId":"df379fe2-3722-4707-93fd-56d41a7b0940","OptionType":"0"},{"OptionName":"否","SelectId":"6ea8bb17-c725-4ed2-853f-29627f94b4bc","TitleId":"1a3bbc99-c818-46e4-a138-a250f014846b","OptionType":"0"},{"OptionName":"是","SelectId":"795abbb5-aed9-4fc6-8c0c-7519bf4b632f","TitleId":"24b30350-ac77-4afa-abda-41d0e88822da","OptionType":"0"},{"OptionName":"否，未接触过","SelectId":"04570d7e-0b63-44d4-b8a2-edce144ee5bd","TitleId":"8bdb0d2b-649c-4798-9d95-9c2b06a0c7c1","OptionType":"0"},{"OptionName":"否","SelectId":"5712563d-eb34-4fd1-a9fe-1b68f57e7e38","TitleId":"be0ed244-91e5-4dbd-a4ab-9c5967145bf3","OptionType":"0"},{"OptionName":"身体状况良好，无异常症状","SelectId":"dd238366-45d4-42e9-abab-40f957c4991e","TitleId":"9e8df714-3e64-4052-9200-766680883e81","OptionType":"0"},{"OptionName":"否","SelectId":"46b00678-b1b5-47e0-b404-00ed9f1a5291","TitleId":"6c4db0fd-bb34-46a1-9f88-4a0315283398","OptionType":"0"}]'
 }
 
-def login(s,login_url,data,user):
+def login(log,s,login_url,data,user):
 
     response = s.post(url=login_url,data=data,headers=headers)
     #print(response)
@@ -285,19 +285,23 @@ def login(s,login_url,data,user):
     # 若返回数据里有 首页 字眼，代表登录成功
     if "首页" in response.text:
         print(user+":1. 登录成功")
+        log.append(user+":登录成功")
     else:
         print(user+":1. 登录失败")
         print((response.text).encode('utf-8'))
+        log.append(user+":登录失败")
 
-def getIndex(s,index_url,logout_url,data,user):
+def getIndex(log,s,index_url,logout_url,data,user):
   
     headers['Referer'] = 'http://xg.swpu.edu.cn/SPCP/Web/Account/ChooseSys'
     response = s.get(url=index_url,headers=headers)
 
     if "当前采集日期已登记！" in response.text:
         print(user+":2. 已登记！")
+        log.append(user+":已登记")
         response = s.get(url=logout_url,headers=headers)
         print(user+":3. 退出登录！")
+        log.append(user+":退出登录")
     else:
         print(user+"2. 发送登记请求！")
         
@@ -306,40 +310,54 @@ def getIndex(s,index_url,logout_url,data,user):
         # 若返回数据里有 已登记 字眼，代表已登记
         if "提交成功！" in response.text:
             print(user+":3. 提交成功！")
+            log.append(user+":提交成功")
             headers['Referer'] = 'http://xg.swpu.edu.cn/SPCP/Web/Account/ChooseSys'
             response = s.get(url=logout_url,headers=headers)
             print(user+":3. 退出登录！")
+            log.append(user+":退出登录")
         else:
             print(user+":3. 提交失败！")
+            log.append(user+":提交失败")
             print((response.text).encode('utf-8'))
             response = s.get(url=logout_url,headers=headers)
             print(user+":3. 退出登录！")
+            log.append(user+":提交失败")
 
+def wirtelog(log):
+    localtime = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) 
+    with open("autosubmit"+localtime+".log","a") as f:                                  #写入log文件
+        for i in range(len(log)):                                                         
+            for j in range(len(log[i])): 
+                f.write(str(log[i][j]))  
+            f.write("\n")
 
 def main():
 
-    login(s1,login_url,data1jhw,"贾昊卫")
-    getIndex(s1,index_url,logout_url,data2jhw,"贾昊卫")
+    log = []
+    login(log,s1,login_url,data1jhw,"贾昊卫")
+    getIndex(log,s1,index_url,logout_url,data2jhw,"贾昊卫")
 
     time.sleep(2)
 
-    login(s2,login_url,data1mmy,"马珉玥")
-    getIndex(s2,index_url,logout_url,data2mmy,"马珉玥")
+    login(log,s2,login_url,data1mmy,"马珉玥")
+    getIndex(log,s2,index_url,logout_url,data2mmy,"马珉玥")
 
     time.sleep(2)
 
-    login(s3,login_url,data1mkf,"穆轲帆")
-    getIndex(s3,index_url,logout_url,data2mkf,"穆轲帆")
-
-    time.sleep(2)
-    
-    login(s4,login_url,data1qn,"秦楠")
-    getIndex(s4,index_url,logout_url,data2qn,"秦楠")
+    login(log,s3,login_url,data1mkf,"穆轲帆")
+    getIndex(log,s3,index_url,logout_url,data2mkf,"穆轲帆")
 
     time.sleep(2)
     
-    login(s5,login_url,data1fj,"付健")
-    getIndex(s5,index_url,logout_url,data2fj,"付健")
+    login(log,s4,login_url,data1qn,"秦楠")
+    getIndex(log,s4,index_url,logout_url,data2qn,"秦楠")
+
+    time.sleep(2)
+    
+    login(log,s5,login_url,data1fj,"付健")
+    getIndex(log,s5,index_url,logout_url,data2fj,"付健")
+    
+    wirtelog(log)
 
 if __name__ == "__main__":
     main()
