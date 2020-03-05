4418667525b884b609ff080c7f78e76d4f1b6bfc
nv4dll-git <nv4dll@outlook.com>
2020-03-05 09:58:08 +0800

    add wenchao

--- autolongin/autosubmit.py
+++ autolongin/autosubmit.py
@@ -20,6 +20,7 @@ s2 = requests.Session()
 s3 = requests.Session()
 s4 = requests.Session()
 s5 = requests.Session()
+s6 = requests.Session()
 
 data1jhw = {
     'StuLoginMode': '1',
@@ -276,6 +277,57 @@ data2fj={
     'PZData': '[{"OptionName":"假期离校回家，目前尚未返校","SelectId":"2e856fa0-5b66-4ba5-97a9-177b4b77e2b3","TitleId":"6564c529-c9ce-4c9c-8158-fcbf53b7ef9a","OptionType":"0"},{"OptionName":"返乡至湖北其他地区(除武汉)","SelectId":"33775f86-44af-4d84-acbe-ddc5f884a48b","TitleId":"0f9eba15-972e-47ce-aff7-691f0c802b78","OptionType":"0"},{"OptionName":"无","SelectId":"d4b03707-0619-4dfb-82c3-c1f2af3bfb53","TitleId":"35eec330-3122-4054-a4ac-fe8ac95135df","OptionType":"0"},{"OptionName":"有","SelectId":"6545bf6d-6b84-4efc-950c-8e79a3eb5b90","TitleId":"bd1fef27-db0a-4898-8963-8b8e34714dfc","OptionType":"0"},{"OptionName":"无","SelectId":"24d314e6-d37d-448d-897e-1910b30f8da1","TitleId":"df379fe2-3722-4707-93fd-56d41a7b0940","OptionType":"0"},{"OptionName":"否","SelectId":"6ea8bb17-c725-4ed2-853f-29627f94b4bc","TitleId":"1a3bbc99-c818-46e4-a138-a250f014846b","OptionType":"0"},{"OptionName":"是","SelectId":"795abbb5-aed9-4fc6-8c0c-7519bf4b632f","TitleId":"24b30350-ac77-4afa-abda-41d0e88822da","OptionType":"0"},{"OptionName":"否，未接触过","SelectId":"04570d7e-0b63-44d4-b8a2-edce144ee5bd","TitleId":"8bdb0d2b-649c-4798-9d95-9c2b06a0c7c1","OptionType":"0"},{"OptionName":"否","SelectId":"5712563d-eb34-4fd1-a9fe-1b68f57e7e38","TitleId":"be0ed244-91e5-4dbd-a4ab-9c5967145bf3","OptionType":"0"},{"OptionName":"身体状况良好，无异常症状","SelectId":"dd238366-45d4-42e9-abab-40f957c4991e","TitleId":"9e8df714-3e64-4052-9200-766680883e81","OptionType":"0"},{"OptionName":"否","SelectId":"46b00678-b1b5-47e0-b404-00ed9f1a5291","TitleId":"6c4db0fd-bb34-46a1-9f88-4a0315283398","OptionType":"0"}]'
 }
 
+data1wc = {
+    'StuLoginMode': '1',
+    'txtUid': '201821000743',
+    'txtPwd': '154555'
+    #'codeInput': '9fmx'
+}
+
+data2wc={
+    'StudentId': '201821000743',
+    'Name': '文超',
+    'MoveTel': '18328070592',
+    'Province': '510000',
+    'City':'510100',
+    'County':'510185',
+    'ComeWhere': '禾丰镇香乐村6组',
+    'FaProvince': '510000',
+    'FaCity': '510100',
+    'FaCounty': '510185',
+    'FaComeWhere': '禾丰镇香乐村6组',
+    'radio_1': '2e856fa0-5b66-4ba5-97a9-177b4b77e2b3',
+    'radio_2': 'ee6648b4-3da8-41ae-b769-04a8d29519e1',
+    'radio_3': 'd4b03707-0619-4dfb-82c3-c1f2af3bfb53',
+    'radio_4': '2ff4ae72-0d80-4428-a919-68e04e9891b9',
+    'radio_5': '24d314e6-d37d-448d-897e-1910b30f8da1',
+    'radio_6': '6ea8bb17-c725-4ed2-853f-29627f94b4bc',
+    'radio_7':'a41b9a56-788a-493b-8d20-df94f8983af0',
+    'radio_8': '04570d7e-0b63-44d4-b8a2-edce144ee5bd',
+    'radio_9': '5712563d-eb34-4fd1-a9fe-1b68f57e7e38',
+    'radio_10': 'dd238366-45d4-42e9-abab-40f957c4991e',
+    'radio_11': '46b00678-b1b5-47e0-b404-00ed9f1a5291',
+    'Other': ' ',
+    'GetAreaUrl': '/SPCP/Web/Report/GetArea',
+    'Sex': '男',
+    'IdCard': '513902199703154555',
+    'SpeType': 'S',
+    'CollegeNo': '000001',
+    'SpeGrade': '2018',
+    'SpecialtyName': '油气田开发工程',
+    'ClassName': '油气田开发工程专业硕2018级05班',
+    'ProvinceName': '四川省',
+    'CityName': '成都市',
+    'CountyName': '简阳市',
+    'FaProvinceName': '四川省',
+    'FaCityName': '成都市',
+    'FaCountyName': '简阳市',
+    'radioCount': '11',
+    'checkboxCount': '0',
+    'blackCount': '0',
+    'PZData': '[{"OptionName":"假期离校回家，目前尚未返校","SelectId":"2e856fa0-5b66-4ba5-97a9-177b4b77e2b3","TitleId":"6564c529-c9ce-4c9c-8158-fcbf53b7ef9a","OptionType":"0"},{"OptionName":"返乡至非湖北地区","SelectId":"ee6648b4-3da8-41ae-b769-04a8d29519e1","TitleId":"0f9eba15-972e-47ce-aff7-691f0c802b78","OptionType":"0"},{"OptionName":"无","SelectId":"d4b03707-0619-4dfb-82c3-c1f2af3bfb53","TitleId":"35eec330-3122-4054-a4ac-fe8ac95135df","OptionType":"0"},{"OptionName":"无","SelectId":"2ff4ae72-0d80-4428-a919-68e04e9891b9","TitleId":"bd1fef27-db0a-4898-8963-8b8e34714dfc","OptionType":"0"},{"OptionName":"无","SelectId":"24d314e6-d37d-448d-897e-1910b30f8da1","TitleId":"df379fe2-3722-4707-93fd-56d41a7b0940","OptionType":"0"},{"OptionName":"否","SelectId":"6ea8bb17-c725-4ed2-853f-29627f94b4bc","TitleId":"1a3bbc99-c818-46e4-a138-a250f014846b","OptionType":"0"},{"OptionName":"否","SelectId":"a41b9a56-788a-493b-8d20-df94f8983af0","TitleId":"24b30350-ac77-4afa-abda-41d0e88822da","OptionType":"0"},{"OptionName":"否，未接触过","SelectId":"04570d7e-0b63-44d4-b8a2-edce144ee5bd","TitleId":"8bdb0d2b-649c-4798-9d95-9c2b06a0c7c1","OptionType":"0"},{"OptionName":"否","SelectId":"5712563d-eb34-4fd1-a9fe-1b68f57e7e38","TitleId":"be0ed244-91e5-4dbd-a4ab-9c5967145bf3","OptionType":"0"},{"OptionName":"身体状况良好，无异常症状","SelectId":"dd238366-45d4-42e9-abab-40f957c4991e","TitleId":"9e8df714-3e64-4052-9200-766680883e81","OptionType":"0"},{"OptionName":"否","SelectId":"46b00678-b1b5-47e0-b404-00ed9f1a5291","TitleId":"6c4db0fd-bb34-46a1-9f88-4a0315283398","OptionType":"0"}]}'
+}
+
 def login(log,s,login_url,data,user):
 
     response = s.post(url=login_url,data=data,headers=headers)
@@ -358,6 +410,10 @@ def main():
     login(log,s5,login_url,data1fj,"付健")
     getIndex(log,s5,index_url,logout_url,data2fj,"付健")
     
+    time.sleep(2)
+
+    login(log,s5,login_url,data1wc,"文超")
+    getIndex(log,s5,index_url,logout_url,data2wc,"文超")
     wirtelog(log)
 
 if __name__ == "__main__":
